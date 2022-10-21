7de145c0618e0fd4d3cb05d090779b6c0a14d59d
aayala4569 <angelicaayala100@gmail.com>
2022-10-20 22:15:24 -0700

    Say-Hello-GIT

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Angelica Ayala
+//Date Revised:10/20/22
+//Mini Challenge #1- "Say Hello"
+//-------Description-----------------
+//I created a string with a variable. 
+//I then output the question "What is your name?". 
+//Then I created variable with user input using Console.ReadLine. 
+//Once provider inputs name, the response "Hello (user name)" will appear.
+
+
+string name;
+
+Console.WriteLine("What is your name?");
+
+name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}.");
