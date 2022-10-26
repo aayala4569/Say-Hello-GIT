5a16f167d448ce86ddb1a717655ae75eece787df
aayala4569 <angelicaayala100@gmail.com>
2022-10-25 22:38:30 -0400

    Updated Say Hello assignment for review.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,53 @@
 //Then I created variable with user input using Console.ReadLine. 
 //Once provider inputs name, the response "Hello (user name)" will appear.
 
-
+//Peer Reviewed by;
+//Date:"
+//Review:
 string name;
+bool playAgain = true;
+bool validResponse = true;
+string userAnswer;
 
-Console.WriteLine("What is your name?");
+while(playAgain == true)
+{
 
-name = Console.ReadLine();
+ Console.WriteLine("What is your name?");
 
-Console.WriteLine($"Hello {name}.");
+ name = Console.ReadLine();
+
+ Console.WriteLine($"Hello {name}.");
+
+ validResponse = true;
+
+ while(validResponse == true)
+        {
+            Console.Write("Do you want to run the program again? (Y/N): "); // console asks if the user wants to run the program again
+            userAnswer = Console.ReadLine().ToUpper();
+            
+
+            if(userAnswer == "Y")
+            {
+                playAgain = true;
+                validResponse = false;
+            }
+            else if(userAnswer == "N")
+            {
+                playAgain = false;
+                validResponse = false;
+            }
+            else
+            {   
+                playAgain = false;
+                validResponse = true;
+                Console.WriteLine(userAnswer + " isn't a valid response. Please Enter an actual valid response please.");
+
+
+            }
+
+
+        }
+    
+        Console.WriteLine("Thanks for playing!");
+}
+
